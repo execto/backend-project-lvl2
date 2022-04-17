877c0fe78868ae1ec54cb5219cf094cd42625561
Aleksander Elakhov <sasha-elahov@yandex.ru>
2022-04-17 18:35:22 +0300

    add args and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 
+import genDiff from '../src/index.js';
+
 const program = new Command();
 program.name('gendiff');
 program.description('Compares two configuration files and shows a difference.');
 program.version('1.0.0');
+
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action(genDiff);
+
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const genDiff = (filepath1, filepath2) => console.log(filepath1, filepath2);
+const genDiff = (filepath1, filepath2, format) => console.log(filepath1, filepath2, format);
 
 export default genDiff;
